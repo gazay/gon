ee68523988969d89a0752f96ac79d657b97e3587
Alexey Gaziev <alex.gaziev@gmail.com>
2012-07-24 15:03:31 +0800

    Added errors for understanding require problems in rabl and jbuilder

--- lib/gon.rb
+++ lib/gon.rb
@@ -48,12 +48,18 @@ class Gon
     end
 
     def rabl(*args)
+      unless Gon.constants.include?(:Rabl)
+        raise "Possible wrong require order problem - try to add `gem 'rabl'` before `gem 'gon'` in your Gemfile"
+      end
       data, options = Gon::Rabl.handler(args)
 
       store_builder_data 'rabl', data, options
     end
 
     def jbuilder(*args)
+      unless Gon.constants.include?(:Jbuilder)
+        raise "Possible wrong require order problem - try to add `gem 'jbuilder'` before `gem 'gon'` in your Gemfile"
+      end
       data, options = Gon::Jbuilder.handler(args)
 
       store_builder_data 'jbuilder', data, options
