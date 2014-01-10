f800ee2a0f2290398b4ff06349e13e9416bdfd1b
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2014-01-09 19:53:27 -0800

    I'm not sure what the previous code was actually supposed to test.  A single mock request was being accessed by multiple threads, each of which was setting a value in a shared hash.  This is not threadsafe, and presumably only passed because of the GIL.  Looking at Travis it appears that this test has always failed on JRuby, and that was only masked because of the Rakefile issue.

--- spec/gon/thread_spec.rb
+++ spec/gon/thread_spec.rb
@@ -1,17 +1,41 @@
 require 'spec_helper'
 
+class GonTestWorker
+  include Gon::GonHelpers
+
+  def request
+    @request ||= ActionDispatch::TestRequest.new
+  end
+
+  def env
+    request.env
+  end
+
+  def execute
+    gon.clear
+    gon.a ||= 1
+    gon.a += 1
+  end
+
+  def value
+    gon.a
+  end
+end
+
 describe 'threading behaviour' do
+  before do
+    Gon.unstub(:current_gon)
+  end
+
   it 'is threadsafe' do
     threads = []
     10.times do
       threads << Thread.new do
-        Gon.clear
-        Gon.a ||= 1
-        Gon.a += 1
-        expect(Gon.a).to eq 2
+        gtw = GonTestWorker.new
+        gtw.execute
+        expect(gtw.value).to eq 2
       end
     end
     threads.each(&:join)
-    expect(Gon.a).to eq 2
   end
 end
